623d285d70e19e221e4558ff02d7dd783c196a16
Lankapalli Pavan Kumar <128818433+pavankumarlankapalli25@users.noreply.github.com>
2024-02-02 22:02:39 +0530

    first commit

---
+++ jenkinsDemo/src/main/java/com/example/demo/JenkinsDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDemoApplication.class, args);
+		System.out.println("Welcome to jenkins..");
+	}
+
+}
